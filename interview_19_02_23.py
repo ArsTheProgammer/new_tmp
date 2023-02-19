1be3b568a70a9eaf6d93174197db351027b882b1
ArsTheProgrammer <amdemidof2009@gmail.com>
2023-02-19 15:17:40 +0300

    again

---
+++ interview_19_02_23.py
@@ -0,0 +1,42 @@
+# number = int(input())
+# if number == 2:
+#    print("verified")
+import random
+
+new_list = list("qwerty")
+print(len(new_list))
+
+from random import randint
+
+randomnum = random.randint(10, 100)
+print(randomnum)
+
+tmp = {"str2": "2"}
+print(tmp)
+
+
+class Temporary():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+num_list = [i for i in range(0, 11, 4)]
+print(num_list)
+
+
+def one():
+    print("!" * 100)
+    copy_list = num_list[:]
+    print(copy_list)
+
+    copy_list.append(9)
+    print(copy_list)
+
+    copy_list.clear()
+    print(copy_list)
+
+# one()
+
